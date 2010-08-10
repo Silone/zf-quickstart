21d0a3307d8abb200d564b8b254f3a54a60a189f
Samuel Herzog <samuel.herzog@salzburg.at>
2010-08-10 08:32:35 +0200

    created db management files & scripts

---
+++ data/scripts/data.sqlite.sql
@@ -0,0 +1,6 @@
+INSERT INTO guestbook
+	( email, comment, created )
+VALUES
+	('samuel.herzog@salzburg.at', 'Did you ever write a guestbook before?', DATETIME('NOW')),
+	('ralph.schindler@zend.com', 'Hello! Hope you enjoy this sample zf application!', DATETIME('NOW'))
+	;
