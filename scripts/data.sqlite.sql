5c1fdd3abe6cd0cf4f5353b0bd65465aa28adfff
Samuel Herzog <samuel.herzog@salzburg.at>
2010-08-11 00:37:01 +0200

    moved scripts folder to top

---
+++ scripts/data.sqlite.sql
@@ -0,0 +1,4 @@
+INSERT INTO guestbook ( email, comment, created )
+VALUES ('samuel.herzog@salzburg.at', 'Did you ever write a guestbook before?', DATETIME('NOW'));
+INSERT INTO guestbook ( email, comment, created )
+VALUES ('ralph.schindler@zend.com', 'Hello! Hope you enjoy this sample zf application!', DATETIME('NOW'));
